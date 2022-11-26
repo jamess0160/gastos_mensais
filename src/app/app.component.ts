dd8d542e908d9a6db3b6facb47022a1def6624a2
jamess0160 <tiagoribeiro12@hotmail.com.br>
2022-11-26 00:25:43 -0300

    finalização do front do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+type registro = {
+	descricao: string,
+	preco: GLfloat,
+	tipo: number
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'gastos_mensais';
+	stylesPrimeiraTabela = {
+		background: "linear-gradient(141.86deg, rgba(255, 122, 0, 0.75) 0%, rgba(138, 5, 190, 0.75) 100%)",
+		width: "calc(75vw - 3rem)",
+		height: "calc(75vh - 3rem)"
+	}
+
+	stylesTabelasInter = {
+		backgroundColor: "#FF7A00",
+		width: "30vw",
+		height: "calc(75vh - 3rem)"
+	}
+
+	stylesTabelasNubank = {
+		backgroundColor: "#8A05BE",
+		width: "30vw",
+		height: "calc(75vh - 3rem)"
+	}
+
+	dadosTotal: registro[] = this.pegarRegistros(1)
+
+	pegarRegistros(tipo: number): registro[] {
+		let registrosSelecionados: registro[] = []
+
+		return registrosSelecionados
+	}
 }
