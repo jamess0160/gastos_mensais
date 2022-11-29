afa33ef55a2da2b33866026ea6c5a8fcee6ca600
jamess0160 <tiagoribeiro12@hotmail.com.br>
2022-11-28 22:26:44 -0300

    criação do serviço de API

---
+++ src/app/api/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { registro } from 'src/tipos';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ApiService {
+
+	constructor(private http: HttpClient) { }
+
+	public pegarGastos(tipo: number): Observable<any> {
+		return this.http.get(`http://localhost:3000/gastos/${tipo}`)
+	}
+
+	public criarGasto(body: registro) {
+		this.http.post(`http://localhost:3000/gastos`, body)
+	}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabelaComponent } from './tabela/tabela.component';
+import { FomularioCriacaoComponent } from './fomulario-criacao/fomulario-criacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabelaComponent
+    TabelaComponent,
+    FomularioCriacaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
