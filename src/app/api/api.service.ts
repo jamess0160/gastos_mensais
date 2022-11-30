f6864c8e23b93de6c7926b6b9e96256e16250db7
jamess0160 <tiagoribeiro12@hotmail.com.br>
2022-11-30 13:17:31 -0300

    .

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -14,7 +14,7 @@ export class ApiService {
 		return this.http.get(`http://localhost:3000/gastos/${tipo}`)
 	}
 
-	public criarGasto(body: registro) {
-		this.http.post(`http://localhost:3000/gastos`, body)
+	public criarGasto(body: registro): Observable<any> {
+		return this.http.post(`http://localhost:3000/gastos`, body)
 	}
 }
