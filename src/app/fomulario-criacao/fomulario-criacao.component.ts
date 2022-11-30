caa918ceeea22eb4c7b6857330445a584e6a387f
jamess0160 <tiagoribeiro12@hotmail.com.br>
2022-11-30 13:17:12 -0300

    finalização da estrutura e função de criar gasto

--- src/app/fomulario-criacao/fomulario-criacao.component.ts
+++ src/app/fomulario-criacao/fomulario-criacao.component.ts
@@ -1,43 +1,95 @@
 import { Component, OnInit } from '@angular/core';
+import { registro } from 'src/tipos';
 import { ApiService } from '../api/api.service';
+
+var classe: any;
 
 @Component({
 	selector: 'app-fomulario-criacao',
 	templateUrl: './fomulario-criacao.component.html',
 	styleUrls: ['./fomulario-criacao.component.scss']
 })
+
 export class FomularioCriacaoComponent implements OnInit {
+
+	teste = 1
 
 	constructor(private api: ApiService) { }
 
 	ngOnInit(): void {
+		classe = this
 		let dialog: any = document.querySelector("#dialog")
+		dialog.addEventListener('cancel', (event: any) => event.preventDefault())
+
+		document.addEventListener('keyup', this.monitorarESC.bind(this))
+	}
+
+	monitorarESC(event: any) {
+		let dialog: any = document.querySelector("#dialog")
+		if (event.key !== "Escape") {
+			return
+		}
+		if (dialog.open) {
+			return this.sair()
+		}
 		dialog.showModal()
 		document.documentElement.style.overflow = 'hidden';
-
-		let inicio: HTMLDivElement | null = document.querySelector("#inicio")
-		document.querySelectorAll("#inicio>button").forEach((item: any) => {
-			item.addEventListener('click', () => {
-				let { chave } = item.dataset
-				if (!inicio) {
-					return
-				}
-				inicio.style.display = 'none'
-				document.querySelector(chave).style.display = "flex"
-			})
-		})
-
-		document.querySelectorAll(".enviar").forEach((item: any) => {
-			let { tipo } = item.dataset
-			if (tipo == "delete") {
-				return this.configurarDelete(item)
-			}
-
-			
-		})
 	}
 
 	configurarDelete(item: any): void {
+
+	}
+
+	sair() {
+		let dialog: any = document.querySelector("#dialog")
+		dialog.close()
+		document.documentElement.style.overflow = 'auto';
+		let divs = document.querySelectorAll("#dialog>div")
+		divs.forEach((div: any, index: number) => index == 0 ? div.style.display = 'flex' : div.style.display = 'none')
+	}
+
+	navegar(chave: string) {
+		let inicio: HTMLDivElement | null = document.querySelector("#inicio")
+		if (!inicio) {
+			return
+		}
+		inicio.style.display = 'none'
+		let destino: any = document.querySelector(chave)
+		destino.style.display = "flex"
+	}
+
+	voltar(event: Event) {
+		let [_, pai]: any = event.composedPath()
+		let inicio: HTMLDivElement | null = document.querySelector("#inicio")
+		if (!inicio) {
+			return
+		}
+		inicio.style.display = 'flex'
+		pai.style.display = 'none'
+	}
+
+	criar(event: SubmitEvent) {
+		event.preventDefault()
+		let [formulario]: any = event.composedPath()
+
+		let formData = new FormData(formulario)
+
+		let valor: number = parseFloat(formData.get('valor')?.toString() || "0")
+		let tipo: number = parseInt(formData.get('tipo')?.toString() || "0")
+
+		this.api.criarGasto({
+			descricao: `${formData.get('data')} - ${formData.get('gasto')}`,
+			preco: valor,
+			tipo: tipo
+		}).subscribe()
+		this.sair()
+	}
+
+	editar() {
+
+	}
+
+	deletar() {
 
 	}
 }
